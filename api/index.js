a79b9461e1f5b316ea598353bffb15bbbedb5573
AFS19 <iireco20@gmail.com>
2023-10-15 18:30:06 +0100

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.DB_URI).then(() => {
+  console.log("DB connected success");
+}).catch((err) => console.log(`Error: ${err}`));
+
 const app = express();
 
 app.listen(3000, () => console.log("Server running"));
