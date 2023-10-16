3219af714d405d95bc226009b5d01b82faf007db
AFS19 <iireco20@gmail.com>
2023-10-16 09:08:40 +0100

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,24 @@
+import mongoose, { model } from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
