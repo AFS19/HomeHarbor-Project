adb16544f8ea85efc4a9c53570feacef66625342
AFS19 <iireco20@gmail.com>
2023-10-16 09:40:29 +0100

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,12 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+
 dotenv.config();
 
-mongoose.connect(process.env.DB_URI).then(() => {
-  console.log("DB connected success");
-}).catch((err) => console.log(`Error: ${err}`));
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => {
+    console.log("DB connected success");
+  })
+  .catch((err) => console.log(`Error: ${err}`));
 
 const app = express();
 
 app.listen(3000, () => console.log("Server running"));
+
+app.use("/api/user", userRouter);
