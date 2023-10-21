afc31688131a9146c355ef5aa4327083b7b1661f
AFS19 <iireco20@gmail.com>
2023-10-22 00:33:42 +0100

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+const PORT = process.env.PORT || 3000;
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -13,7 +15,11 @@ mongoose
   .catch((err) => console.log(`Error: ${err}`));
 
 const app = express();
+app.use(express.json());
 
-app.listen(3000, () => console.log("Server running"));
+app.listen(PORT, () =>
+  console.log(`Server running at http://localhost:${PORT}`)
+);
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
