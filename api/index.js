ee066e7969c6947d2c98ba614f2860af1700934b
AFS19 <iireco20@gmail.com>
2023-10-23 19:32:34 +0100

    complete sign up page functionnality

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 
 app.listen(PORT, () =>
-  console.log(`Server running at http://localhost:${PORT}`)
+  console.log(`Server running at http://127.0.0.1:${PORT}`)
 );
 
 app.use("/api/user", userRouter);
--- client/src/pages/SignUp.1.jsx
+++ client/src/pages/SignUp.1.jsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
-
+  // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(false);
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -14,31 +12,8 @@ export default function SignUp() {
     });
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-
-    try {
-      setLoading(true);
-      const res = await fetch("/api/auth/signup", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
-      const data = await res.json();
-      if (data.success === false) {
-        setLoading(false);
-        setError(data.message);
-      }
-      setLoading(false);
-      setError(null);
-      navigate("/sign-in");
-    } catch (error) {
-      setLoading(false);
-      setError(error.message);
-      console.log(error.message);
-    }
   };
 
   return (
@@ -70,18 +45,15 @@ export default function SignUp() {
           onChange={handleChange}
         />
         {/* <input
-          type="password"
-          name="confirmPassword"
-          id="confirmPassword"
-          placeholder="confirm password"
-          className="border rounded-lg p-3"
-          onChange={handleChange}
-        /> */}
-        <button
-          disabled={loading}
-          className="bg-slate-700 text-white p-3 uppercase rounded-lg hover:scale-95 hover:opacity-90"
-        >
-          {loading ? "Loading..." : "Sign up"}
+              type="password"
+              name="confirmPassword"
+              id="confirmPassword"
+              placeholder="confirm password"
+              className="border rounded-lg p-3"
+              onChange={handleChange}
+            /> */}
+        <button className="bg-slate-700 text-white p-3 uppercase rounded-lg hover:scale-95 hover:opacity-90">
+          "Sign up"
         </button>
       </form>
 
@@ -90,8 +62,8 @@ export default function SignUp() {
         <Link to="/sign-in">
           <span className="text-blue-700">Sign in</span>
         </Link>
+        {error && <p className="text-red-700 mt-5">{error.message}</p>}
       </div>
-      {error && <p className="text-red-700 mt-5">{error}</p>}
     </div>
   );
 }
