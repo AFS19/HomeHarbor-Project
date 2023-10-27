89edb81db13b7e9fb97a4f5843f9cb578926242b
AFS19 <iireco20@gmail.com>
2023-10-28 00:38:16 +0100

    complete ading  Google authentication functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -35,3 +35,48 @@ export const signin = async (req, res, next) => {
     next(err);
   }
 };
+
+export const google = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (user) {
+      const token = Jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const { password: pass, ...rest } = user._doc;
+
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+          expire: new Date() + 24 * 3600 * 1000,
+        })
+        .status(200)
+        .json(rest);
+    } else {
+      const generatePassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hashPassword = bcrypt.hashSync(generatePassword, 10);
+      const newUser = new User({
+        username:
+          req.body.name.split(" ").join("") +
+          Math.random().toString(36).slice(-4),
+        email: req.body.email,
+        password: hashPassword,
+        avatar: req.body.avatar,
+      });
+      await newUser.save();
+
+      const token = Jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const { password: pass, ...rest } = newUser._doc;
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+          expire: new Date() + 24 * 3600 * 1000,
+        })
+        .status(200)
+        .json(rest);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,7 +1,13 @@
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "@firebase/auth";
 import { app } from "../firebase";
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../redux/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 export default function OAuth() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const handleGoogleClick = async () => {
     try {
       const provider = new GoogleAuthProvider();
@@ -10,7 +16,22 @@ export default function OAuth() {
 
       const result = await signInWithPopup(auth, provider);
 
-      console.log(result);
+      const res = await fetch("/api/auth/google", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          avatar: result.user.photoURL,
+        }),
+      });
+
+      const data = await res.json();
+
+      dispatch(signInSuccess(data));
+      navigate("/");
     } catch (error) {
       console.log("could not sign in with google", error);
     }
