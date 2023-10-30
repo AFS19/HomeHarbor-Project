ec9cbc88d5bd1acb49ccf334606f48ba04451962
AFS19 <iireco20@gmail.com>
2023-10-30 22:59:11 +0000

    start working on image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,13 +1,68 @@
 import { useSelector } from "react-redux";
+import { useEffect, useRef, useState } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 export default function Profile() {
   const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+
+  console.log(formData);
+  console.log(filePerc);
+  console.log(fileUploadError);
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
+          setFormData({ ...formData, avatar: downloadUrl });
+        });
+      }
+    );
+  };
 
   return (
     <div className="max-w-lg mx-auto p-5 border shadow-xl mt-20">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-      <form className="flex flex-col gap-5">
+      <form className="flex flex-col gap-5" encType="multipart/form-data">
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          hidden
+          type="file"
+          ref={fileRef}
+          accept="image/*"
+        />
         <img
+          onClick={() => fileRef.current.click()}
           src={currentUser.avatar}
           className="rounded-full w-30 h-30 object-cover cursor-pointer self-center mb-5"
           alt="profile"
@@ -17,7 +72,6 @@ export default function Profile() {
           type="text"
           name="username"
           id="username"
-          value={currentUser.username}
           className="border rounded-lg p-3"
           placeholder="username"
         />
@@ -25,7 +79,6 @@ export default function Profile() {
           type="text"
           name="email"
           id="email"
-          value={currentUser.email}
           className="border rounded-lg p-3"
           placeholder="email"
         />
