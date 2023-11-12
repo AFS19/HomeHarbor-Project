f3bf147f5218845020fbe11a868756815ba8e062
AFS19 <iireco20@gmail.com>
2023-11-12 21:01:24 +0000

    complete create listing page UI

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -4,7 +4,7 @@ export default function CreateListing() {
       <h1 className="text-3xl font-semibold text-center my-7">
         Create Listing
       </h1>
-      <form className="flex flex-col sm:flex-row gap-10">
+      <form className="flex flex-col sm:flex-row gap-4">
         <div className="flex flex-col gap-4 flex-1">
           <input
             type="text"
@@ -33,7 +33,7 @@ export default function CreateListing() {
             maxLength="50"
             required
           />
-          <div className="flex gap-6 flex-wrap">
+          <div className="flex flex-wrap gap-6">
             <div className="flex gap-2">
               <input type="checkbox" name="sell" id="sell" className="w-5" />
               <label htmlFor="sell">Sell</label>
@@ -65,31 +65,139 @@ export default function CreateListing() {
               <label htmlFor="offer">Offer</label>
             </div>
           </div>
-          <div className="flex gap-4">
-            <div className="">
+          <div className="flex flex-wrap gap-6">
+            <div className="flex items-center gap-2">
+              <input
+                type="number"
+                id="bedrooms"
+                min="1"
+                max="10"
+                required
+                className="p-3 border border-gray-300 rounded-lg"
+              />
+              <p>Beds</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <input
+                type="number"
+                id="bathrooms"
+                min="1"
+                max="10"
+                required
+                className="p-3 border border-gray-300 rounded-lg"
+              />
+              <p>Baths</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <input
+                type="number"
+                id="regularPrice"
+                min="50"
+                max="10000000"
+                required
+                className="p-3 border border-gray-300 rounded-lg"
+              />
+              <div className="flex flex-col items-center">
+                <p>Regular price</p>
+                <span className="text-xs">($ / month)</span>
+              </div>
+            </div>
+            <div className="flex items-center gap-2">
+              <input
+                type="number"
+                id="discountPrice"
+                min="0"
+                max="10000000"
+                required
+                className="p-3 border border-gray-300 rounded-lg"
+              />
+              <div className="flex flex-col items-center">
+                <p>Discounted price</p>
+
+                <span className="text-xs">($ / month)</span>
+              </div>
+            </div>
+          </div>
+          {/* <div className="flex flex-wrap gap-6">
+            <div className="flex items-center gap-2">
               <input
                 type="number"
                 name="bedrooms"
                 id="bedrooms"
                 min="1"
                 max="20"
+                required
+                className="p-3 border border-gray-300 rounded-lg"
               />
               <label htmlFor="bedrooms">Beds</label>
             </div>
-            <div className="">
+            <div className="flex items-center gap-2">
               <input
                 type="number"
                 name="bathrooms"
                 id="bathrooms"
                 min="1"
                 max="20"
+                required
+                className="p-3 border border-gray-300 rounded-lg"
               />
               <label htmlFor="bathrooms">Baths</label>
             </div>
-          </div>
+            <div className="flex items-center gap-2">
+              <input
+                type="number"
+                name="regularPrice"
+                id="regularPrice"
+                min="1"
+                max="20"
+                required
+                className="p-3 border border-gray-300 rounded-lg"
+              />
+              <div className="">
+                <label htmlFor="regularPrice">Regular price</label>
+                <span className="text-xs">($ / month)</span>
+              </div>
+            </div>
+            <div className="flex items-center gap-2">
+              <input
+                type="number"
+                name="discountedPrice"
+                id="discountedPrice"
+                min="1"
+                max="20"
+                required
+                className="p-3 border border-gray-300 rounded-lg"
+              />
+              <div className="">
+                <label htmlFor="discountedPrice">Discounted price</label>
+                <span className="text-xs">($ / month)</span>
+              </div>
+            </div>
+          </div> */}
         </div>
-        <div className="flex flex-col gap-4 flex-1">
-          <input type="file" name="" id="" />
+        <div className="flex flex-col flex-1 gap-4">
+          <p className="font-semibold">
+            Images:
+            <span className="font-normal tex-gray-600 ml-2">
+              The first image will be the cover (max6)
+            </span>
+          </p>
+          <div className="flex gap-4">
+            <input
+              type="file"
+              name="images"
+              id="images"
+              accept="image/*"
+              multiple
+              className="p-3 border border-gray-300 rounded w-full"
+            />
+            <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">
+              Upload
+            </button>
+          </div>
+          <button className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:scale-95 hover:opacity-90 disabled:opacity-80">
+            Create listing
+          </button>
         </div>
       </form>
     </main>
