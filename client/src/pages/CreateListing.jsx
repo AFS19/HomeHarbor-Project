36a49242c4bb9cf5a0d8e2b7b042570f7885e7b5
AFS19 <iireco20@gmail.com>
2023-11-15 21:16:25 +0000

    complete upload listing images functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,11 +1,62 @@
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 import { useState } from "react";
+import { app } from "../firebase";
 
 export default function CreateListing() {
   const [files, setFiles] = useState([]);
+  const [formData, setFormData] = useState({
+    imagesUrls: [],
+  });
+
+  console.log(formData);
 
   const handleImageUpload = (e) => {
     e.preventDefault();
-    console.log(files);
+
+    if (files.length > 0 && files.length < 7) {
+      const promises = [];
+
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[i]));
+      }
+
+      Promise.all(promises).then((urls) => {
+        setFormData({
+          ...formData,
+          imagesUrls: formData.imagesUrls.concat(urls),
+        });
+      });
+    }
+
+    const storeImage = async (file) => {
+      return new Promise((resolve, reject) => {
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + "_" + file.name;
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log(progress);
+          },
+          (error) => {
+            reject(error);
+          },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              resolve(downloadURL);
+            });
+          }
+        );
+      });
+    };
   };
 
   return (
@@ -148,6 +199,7 @@ export default function CreateListing() {
             />
             <button
               onClick={handleImageUpload}
+              type="button"
               className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80"
             >
               Upload
