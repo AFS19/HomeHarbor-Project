8fb6977db5a3d3da11b4cf9a4023e3f85cae4c01
AFS19 <iireco20@gmail.com>
2023-12-04 12:56:23 +0000

    complete adding image slider to the listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,14 +1,19 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
 
 export default function Listing() {
   const params = useParams();
-  const [listing, setListing] = useState();
+  const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+  SwiperCore.use([Navigation]);
 
   useEffect(() => {
-    const listingId = params.paramsId;
+    const listingId = params.listingId;
     const fetchListing = async () => {
       try {
         setLoading(true);
@@ -27,7 +32,32 @@ export default function Listing() {
       }
     };
     fetchListing();
-  }, []);
+  }, [params.listingId]);
 
-  return <div>{listing && listing.name}</div>;
+  return (
+    <main>
+      {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
+      {error && (
+        <p className="text-center my-7 text-sm text-red-700">{error}</p>
+      )}
+
+      {listing && !loading && !error && (
+        <>
+          <Swiper navigation>
+            {listing.imageUrls.map((url, index) => (
+              <SwiperSlide key={index}>
+                <div
+                  className="h-[550px]"
+                  style={{
+                    background: `url(${url}) center no-repeat`,
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </>
+      )}
+    </main>
+  );
 }
