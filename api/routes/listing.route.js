242428a86559b595fcd1bcee6c544140b3c8710a
AFS19 <iireco20@gmail.com>
2023-12-08 20:20:20 +0000

    create search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -5,6 +5,7 @@ import {
   deleteListing,
   updateListing,
   get,
+  getListings,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -12,5 +13,6 @@ router.post("/create", verifyToken, create);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
 router.get("/get/:id", get);
+router.get("/get", getListings);
 
 export default router;
