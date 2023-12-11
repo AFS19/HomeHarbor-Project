70f50d4bed325d50f2c3e0992efeeb8c9037d9dd
AFS19 <iireco20@gmail.com>
2023-12-11 15:57:58 +0000

    complete header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,29 @@
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const urlQueries = urlParams.toString();
+    navigate(`/search?${urlQueries}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, []);
+
   return (
     <header className="bg-slate-200 shadow-md w-full">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -13,13 +33,20 @@ export default function Header() {
             <span className="text-slate-700">Harbor</span>
           </h1>
         </Link>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 p-3 rounded-lg flex items-center focus:bg-red-900"
+        >
           <input
             type="text"
+            value={searchTerm}
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <FaSearch className="text-slate-600" />
+          <button>
+            <FaSearch className="text-slate-600 hover:scale-75" />
+          </button>
         </form>
         <ul className="flex gap-4">
           <Link to="/">
