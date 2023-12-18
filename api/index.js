001ec354c862623f48f9781e26d332fdf10e7ec4
AFS19 <iireco20@gmail.com>
2023-12-18 19:22:53 +0000

    add pre installation to be ready for deploy

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
@@ -15,6 +16,8 @@ mongoose
     console.log("DB connected success");
   })
   .catch((err) => console.log(`Error: ${err}`));
+
+const __direname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -27,6 +30,12 @@ app.listen(PORT, () =>
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__direname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__direname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
